a59a0eb51a5610b0dec8de254613b19195e33fb2
André-git <andre.potthast.ap@gmail.com>
2022-11-13 14:41:48 +0100

    Exercise 04 first try

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...toRemove) {
 
-};
-
+return array
+        .filter((element)=>{
+        return(!toRemove.includes(element))
+});
+}
 // Do not edit below this line
 module.exports = removeFromArray;
