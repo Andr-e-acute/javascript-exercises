d810fc1056cc9d7b3285423bcb113b4c2b6fde82
André-git <andre.potthast.ap@gmail.com>
2022-11-13 13:47:29 +0100

    recommit exercise 2 done
    I think the last commit has not worked

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,repeatAmount) {
+    if(repeatAmount<0){return 'ERROR'}
+    let repeatedString=""
+    for (let index = 0; index < repeatAmount ; index++) {
+        repeatedString+=string
+        
+    }
+    return repeatedString
+    
 };
-
+ repeatString();
 // Do not edit below this line
 module.exports = repeatString;
