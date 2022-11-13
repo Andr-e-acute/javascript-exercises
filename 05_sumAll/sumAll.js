5ecf3fa6fbc2ae7698ecf9769e29e4ae73e42b97
André-git <andre.potthast.ap@gmail.com>
2022-11-13 15:34:33 +0100

    Exercise 05 passed all Test but it is messy

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (...numbers) {
+  if([...numbers].some((number)=>{
+    return number<0||(typeof number !== 'number')
+  })){
+    return "ERROR"
+  }
+  let biggerNum = Math.max(...numbers);
+  let smallerNum = Math.min(...numbers);
+  let sum = biggerNum;
 
+  for (let index = smallerNum; index < biggerNum; index++) {
+    sum += index;
+  }
+  return sum;
 };
 
 // Do not edit below this line
