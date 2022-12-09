57885da40c4796a5bd3ddc5850670f910c87f1b5
André-git <andre.potthast.ap@gmail.com>
2022-12-09 14:26:55 +0100

    first solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+//remove all non word characters and lowercase them 
+const  cleanStr = str.toLowerCase().replace(/[^a-z]/g,"")
+//split to array reverse and join
+let reversedCleanStr=cleanStr
+    .split("")
+    .reverse()
+    .join("");
+//compare str to reversed str 
+return (cleanStr==reversedCleanStr)
 };
 
 // Do not edit below this line
