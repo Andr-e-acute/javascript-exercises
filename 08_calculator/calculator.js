37c19bb114ef72747e50b102411c8a80fc2e563f
André-git <andre.potthast.ap@gmail.com>
2022-12-09 14:04:36 +0100

    merged with recursive Solution branch

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,38 +1,37 @@
-const add = function(num1,num2) {
-	return num1+num2
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function(num1,num2) {
-	return num1 -num2
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function(numArr) {
-	let sum =0;
-  for (let num of numArr){
-    sum = sum + Number(num)
+const sum = function (numArr) {
+  let sum = 0;
+  for (let num of numArr) {
+    sum = sum + Number(num);
   }
-  return sum
+  return sum;
 };
 
-const multiply = function(numArr) {
-  let sum=1;
-  for (let num of numArr){
-    sum = sum* num
+const multiply = function (numArr) {
+  let sum = 1;
+  for (let num of numArr) {
+    sum = sum * num;
   }
- return sum
+  return sum;
 };
 
-const power = function(num1, num2) {
-	return num1 ** num2;
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function(num) {
-if(num==0){
-  return 1
-}
-else{
-  return num * factorial(num-1)
-}
+const factorial = function (num) {
+  if (num == 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
@@ -42,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
