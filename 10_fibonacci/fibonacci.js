e9c3ca11a415d7136cb4de313045297176a4b08d
André-git <andre.potthast.ap@gmail.com>
2022-12-09 14:47:28 +0100

    first solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+// 1, 1, 2, 3, 5, 8,
+const fibonacci = function (member) {
+  if(member <0) return "OOPS"
+  let lastNumber = 1;
+  let secondLastNumber = 0;
+  let result = 1;
 
+
+  for (let i = 1; i < member; i++) {
+    result = lastNumber + secondLastNumber;
+    secondLastNumber = lastNumber;
+    lastNumber = result;
+  }
+  return result;
 };
 
 // Do not edit below this line
